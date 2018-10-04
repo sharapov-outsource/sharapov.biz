974933c55ee36e11745aee1a9cd4332b90586e65
Alexander S <alexander@sharapov.biz>
2018-10-04 22:39:00 +0600

    removing logging functionality

--- src/settings.php
+++ src/settings.php
@@ -7,13 +7,6 @@ return [
         // Renderer settings
         'renderer' => [
             'template_path' => __DIR__ . '/../templates/',
-        ],
-
-        // Monolog settings
-        'logger' => [
-            'name' => 'slim-app',
-            'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/app.log',
-            'level' => \Monolog\Logger::DEBUG,
-        ],
+        ]
     ],
 ];
